6028b4e06ba18601ffcf60927132119663653f86
Touko Hänninen <touko.j.hanninen@student.jyu.fi>
2025-12-02 15:59:12 +0200

    lisätty optionaaliset kentät

--- src/database.py
+++ src/database.py
@@ -3,7 +3,8 @@ import sqlite3
 class Database:
     def __init__(self, db_nimi = ":memory:"):
         self.db_nimi = db_nimi
-        self.conn = sqlite3.connect(self.db_nimi)
+        self.conn = sqlite3.connect(self.db_nimi,  check_same_thread=False)
+        self.conn.row_factory = sqlite3.Row
         self.cursor = self.conn.cursor()
         print("Yhdistetty tietokantaan: ", self.db_nimi)
 
@@ -15,21 +16,51 @@ class Database:
             type TEXT NOT NULL,
             author TEXT NOT NULL,
             title TEXT NOT NULL,
-            year INTEGER NOT NULL
+            year INTEGER NOT NULL,
+            booktitle TEXT,
+            journal TEXT,
+            volume TEXT,
+            pages TEXT,
+            publisher TEXT                
         )                    
         """)
         self.conn.commit()
 
     def insert_defaults(self):
         self.cursor.execute("""
-        INSERT OR IGNORE INTO viitteet (viite, type, author, title, year) 
+        INSERT OR IGNORE INTO viitteet (viite, type, author, title, year, booktitle) 
                             VALUES (
                             "VPL11", 
                             "inproceedings", 
                             "Vihavainen, Arto and Paksula, Matti and Luukkainen, Matti",
                             "Extreme Apprenticeship Method in Teaching Programming for Beginners.",
-                            2011)                
+                            2011,
+                            "SIGCSE '11: Proceedings of the 42nd SIGCSE technical symposium on Computer science education")                
         """)
+
+        self.cursor.execute("""
+        INSERT OR IGNORE INTO viitteet (viite, type, author, title, year, publisher) 
+                            VALUES (
+                            "Martin09", 
+                            "book", 
+                            "Martin, Robert",
+                            "Clean Code: A Handbook of Agile Software Craftsmanship",
+                            2008,
+                            "Prentice Hall")                
+        """)
+        self.cursor.execute("""
+        INSERT OR IGNORE INTO viitteet (viite, type, author, title, year, journal, volume, pages) 
+                            VALUES (
+                            "CBH91", 
+                            "article", 
+                            "Allan Collins and John Seely Brown and Ann Holum",
+                            "Cognitive apprenticeship: making thinking visible",
+                            1991,
+                            "American Educator",
+                            "6",
+                            "38--46")                
+        """)
+        
         self.conn.commit()
 
     def lisaa_viite(self, data: dict):
--- src/tests/database_test.py
+++ src/tests/database_test.py
@@ -11,8 +11,17 @@ class TestDB(unittest.TestCase):
         self.db.conn.close()
 
     def test_lisatty_viite_on_tietokannassa(self):
-        uusi = {"viite": "joku123", "type": "inproceedings", "author": "jonne", "title": "luokat ovat olioita", "year": 2025}
+        uusi = {"viite": "joku123", 
+                "type": "inproceedings", 
+                "author": "jonne", 
+                "title": "luokat ovat olioita", 
+                "year": 2025}
 
         self.db.lisaa_viite(uusi)
         rivi = self.db.hae_viite("joku123")
         self.assertEqual(uusi["viite"], rivi[1])
+
+    def test_kursorin_kenttiin_voi_viitata_nimella(self):
+        rivi = self.db.hae_viite("VPL11")
+
+        self.assertEqual(rivi ["type"], "inproceedings")
