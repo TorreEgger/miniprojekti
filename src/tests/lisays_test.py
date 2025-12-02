2fa6ce0bce4c86d300784a385934cd186dad2cd2
oneemasa <eevert.o.m.salminen@student.jyu.fi>
2025-12-02 09:46:23 +0200

    lisäyksen eka testi

---
+++ src/tests/lisays_test.py
@@ -0,0 +1,31 @@
+import unittest
+from database import Database
+
+
+class TestLisays(unittest.TestCase):
+        
+
+     #Otetaan tieotkanta käyttöön ja alustetaan
+    def setUp(self):
+        self.db = Database(":memory:")
+        self.db.create_table()
+        
+
+
+    #käytännössä sama kuin poistossa, mutta ilman poistamista
+    def test_lisays_onnistuu(self):
+            
+        
+            self.db.lisaa_viite({
+            "viite": "JJQ12",
+            "type": "book",
+            "author": "Matti Martikainen",
+            "title": "Parhaat kesäkaupungit",
+            "year": 1988              
+            })
+
+            viite = self.db.hae_viite("JJQ12")
+            self.assertEqual(viite[1], "JJQ12")
+           
+
+    
